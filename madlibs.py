a1d7d84b4d332a021d1b2c740c6429fbbc097196
Khushmita Kaur <khushp9894@gmail.com>
2024-01-23 23:43:47 -0800

    added code

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+print("Please enter one word answers for the following prompts:")
+
+Adj1 = input("Adjective: ")
+verb1 = input("Verb: ")
+Adj2 = input("Adjective: ")
+verb3 = input("Verb: ")
+Adj3 = input("Adjective: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {Adj1}! I have always wanted to {verb1} and do {Adj2} things with it. \
+It makes me so excited all the time because I love to {verb3}. I would like to become {Adj3} \
+like {famous_person} in their field." 
+
+print(madlib)
